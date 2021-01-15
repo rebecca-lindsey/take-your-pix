4d223d1ca9bcc2b127716ba75b9939df0e2c90cd
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-01-15 16:24:03 -0500

    Create layout and login pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,8 +7,12 @@ module ApplicationHelper
     current_photographer ||= Photographer.find_by(id: session[:photographer_id])
   end
 
+  def logged_in?
+    current_photographer || current_client
+  end
+
   def require_login
-    redirect_to login_path unless current_photographer || current_client
+    redirect_to login_path unless logged_in?
   end
 
   def require_photographer
