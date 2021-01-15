5ee8ee84cde162daa6769f7fcc6a3346bb50b8cf
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-01-15 15:53:23 -0500

    Add helper method for current users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def current_photographer
+    current_photographer ||= Photographer.find_by(id: session[:photographer_id])
+  end
+
+  def current_client
+    current_photographer ||= Photographer.find_by(id: sesssion[:photographer_id])
+  end
 end
