7d439765cc5db7e3880968feeb994d01d96ba995
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-01-15 17:12:39 -0500

    Set up photographer sign up, style field_with_errors

--- app/controllers/photographers_controller.rb
+++ app/controllers/photographers_controller.rb
@@ -1,6 +1,27 @@
 class PhotographersController < ApplicationController
-    skip_before_action :require_login
+  skip_before_action :require_login
+
+  def new
+    @photographer = Photographer.new
+  end
+
+  def create
+    @photographer = Photographer.create(photographer_params)
+    if @photographer.valid?
+      byebug
+      redirect_to photographer_path(@photographer)
+    else
+      render new_photographer_path
+    end
+  end
+
   def show
     @photographer = current_photographer
+  end
+
+  private
+
+  def photographer_params
+    params.require(:photographer).permit(:username, :password, :email, :location, :specialty)
   end
 end
--- app/models/photographer.rb
+++ app/models/photographer.rb
@@ -2,4 +2,7 @@ class Photographer < ApplicationRecord
   has_many :albums
   has_many :clients, through: :albums
   has_secure_password
+
+  validates :email, presence: true, uniqueness: true
+  validates :username, :password, :location, :specialty, presence: true
 end
