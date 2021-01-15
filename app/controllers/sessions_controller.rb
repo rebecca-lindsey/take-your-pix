8dda90ece9d9695a3f5757d9bf0e36007d175d27
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-01-15 14:48:39 -0500

    Set up basic login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,8 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    if @photographer = Photographer.find_by(email: params[:user][:email])
+    byebug
+    if @photographer = Photographer.find_by(email: params[:user][:email])&.authenticate(params[:user][:password])
       session[:user_id] = @photographer.id
       redirect_to photographer_path(@photographer)
     elsif @client = Client.find_by(email: params[:user][:email])
