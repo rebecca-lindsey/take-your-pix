5220db600150b28a0feee010033c2b64db654bf6
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-01-15 23:07:24 -0500

    Set up destroy action and edit links for Photographer

--- app/models/photographer.rb
+++ app/models/photographer.rb
@@ -1,9 +1,8 @@
 class Photographer < ApplicationRecord
-  has_many :albums
+  has_many :albums, dependent: :destroy
   has_many :clients, through: :albums
   has_secure_password
 
   validates :email, presence: true, uniqueness: true
   validates :username, :password, :password_confirmation, :location, :specialty, presence: true
-
 end
