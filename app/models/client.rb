1a59e5d42ee9d3f4b0b7b64fd1dd9877e9b4db6b
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-01-15 23:17:55 -0500

    Add Client#destroy, correct typos

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,8 @@
 class Client < ApplicationRecord
-    has_many :albums
-    has_many :photographers, through: :albums
-    has_secure_password
+  has_many :albums, dependent: :destroy
+  has_many :photographers, through: :albums
+  has_secure_password
+
+  validates :email, presence: true, uniqueness: true
+  validates :username, :password, :password_confirmation, :location, presence: true
 end
