94c0891f05381c79e2d73ad19ecf8b1a2ff77647
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-01-15 21:53:10 -0500

    Create display_all_photos method

--- app/helpers/albums_helper.rb
+++ app/helpers/albums_helper.rb
@@ -1,2 +1,9 @@
 module AlbumsHelper
+  def display_all_photos
+    tag.ul do
+      @photos.each do |photo|
+        concat tag.li(link_to(photo.title, photo_path(photo)))
+      end
+    end
+  end
 end
