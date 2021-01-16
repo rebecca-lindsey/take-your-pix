a8e8aabab6659b617edf0e7f338b60aa190486bf
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-01-15 22:01:02 -0500

    Update PhotosController

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,48 @@
 class PhotosController < ApplicationController
+  include PhotosHelper
+
+  def new
+    @photo = Photo.new
+    @album = current_album
+  end
+
+  def create
+    @album = current_album
+    if current_album.photos.build(photo_params).save
+      @photo = current_album.photos.last
+      redirect_to photo_path(@photo)
+    else
+      render new_photo_path
+    end
+  end
+
+  def show
+    @photo = Photo.find_by(id: params[:id])
+    @album = @photo.album
+  end
+
+  def edit
+    @photo = Photo.find_by(id: params[:id])
+    @album = @photo.album
+  end
+
+  def update
+    @photo = Photo.find_by(id: params[:id])
+    @photo.update(photo_params)
+    if @photo.valid?
+      redirect_to photo_path(@photo)
+    else
+      render edit_photo_path
+    end
+  end
+
+  private
+
+  def current_album
+    Album.find_by(id: params[:id])
+  end
+
+  def photo_params
+    params.require(:photo).permit(:title, :description, :date, :location, :album_id)
+  end
 end
