5a5fe0bea1c7c111e861885fe8253eb8182bc655
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-01-15 22:28:20 -0500

    Update into nested resource

--- app/helpers/albums_helper.rb
+++ app/helpers/albums_helper.rb
@@ -2,7 +2,7 @@ module AlbumsHelper
   def display_all_photos
     tag.ul do
       @photos.each do |photo|
-        concat tag.li(link_to(photo.title, photo_path(photo)))
+        concat tag.li(link_to(photo.title, album_photo_path(@album, photo)))
       end
     end
   end
