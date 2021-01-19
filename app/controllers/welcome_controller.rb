421c47cd4770898d9c76841ae279d98d10c60e65
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-01-19 13:06:17 -0500

    Add multi-account sign in and refactor Sessions#create

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,10 @@
 class WelcomeController < ApplicationController
   skip_before_action :require_login
-  # layout 'guest'
-  def index
+  include WelcomeHelper
+  def index; end
+
+  def create
+    session[:account_type] = params[:user][:account_type]
+    redirect_to login_path
   end
 end
