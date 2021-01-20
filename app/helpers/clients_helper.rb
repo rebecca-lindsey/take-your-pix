9dc6fb6293cc81e63f5c7252b92b7a466ef28804
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-01-20 13:14:04 -0500

    Add view control based on client, update client helper

--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -2,7 +2,7 @@ module ClientsHelper
   def display_all_clients
     tag.ul do
       @clients.each do |client|
-        concat tag.li(link_to(client.username, photographer_path(client))) unless client == current_client
+        concat tag.li(link_to(client.username, client_path(client))) unless client == current_client
       end
     end
   end
