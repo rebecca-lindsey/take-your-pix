586457038a315059d3141e729ccb76e585333f67
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-01-21 17:14:01 -0500

    Remove byebug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -24,7 +24,6 @@ class SessionsController < ApplicationController
     user = account_type.constantize.from_omniauth(request.env['omniauth.auth'])
     if user.valid?
       session[:user_id] = user.id
-      byebug
       if account_type == 'Photographer'
         redirect_to photographer_path(user)
       else
