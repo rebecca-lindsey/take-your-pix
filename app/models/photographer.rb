1f8b3565ff523a8350fee082f6a92b978311e1e6
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-01-21 16:57:30 -0500

    Cleaned up Best Loved and added link

--- app/models/photographer.rb
+++ app/models/photographer.rb
@@ -6,4 +6,12 @@ class Photographer < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true
   validates :password_digest, presence: true
+
+  def self.with_most_albums(num)
+    joins(:albums).group('photographer_id').having("COUNT(*) == #{num}")
+  end
+
+  def self.highest_album_count
+    joins(:albums).group('photographer_id').count.values[0]
+  end
 end
