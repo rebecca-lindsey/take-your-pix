e095dc76fb0154cf996926fbde5cbf0f1bbb58fa
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2021-01-22 00:11:46 -0500

    Make sure form errors dispaly on Album, validate max lengths

--- app/models/album.rb
+++ app/models/album.rb
@@ -3,8 +3,10 @@ class Album < ApplicationRecord
   belongs_to :client
   has_many :photos, dependent: :destroy
 
-  validates :title, presence: true, length: { maximum: 30,
-                                              too_long: 'Album title must be less than 30 characters' }
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :date, presence: true
+  validates :description, length: { maximum: 255 }
+  validates :location, length: { maximum: 50 }
 
   def to_param
     "#{id}-#{title.parameterize}"
